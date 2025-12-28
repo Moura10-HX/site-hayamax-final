35d0986507dd4598ac9ee76dd02d033354d1618b
Moura10-HX <emourajunior@hotmail.com>
2025-12-27 21:02:23 -0300

    Feat: Criando Home Page na raiz para evitar 404

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  // Assim que alguém entrar na raiz, joga para o login
-  redirect("/login");
+  redirect("/acesso");
 }
